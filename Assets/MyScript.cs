bb3a390bc210f71d336aa6059ca28d7b867d02fb
Sohan <sohan.sarabu@gmail.com>
2016-08-11 16:28:24 -0500

    Added script

---
+++ Assets/MyScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("I just pooped out a watermelon!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
