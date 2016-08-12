cc324743a2189f0612146234f75ec47af8195cca
Sohan <sohan.sarabu@gmail.com>
2016-08-12 06:44:13 -0500

    Creating a replay system

---
+++ Assets/Scripts/Replay.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Replay : MonoBehaviour {
+
+    private const int bufferFrames = 100;
+    private MyKeyFrame[] keyFrames = new MyKeyFrame [bufferFrames];
+
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Record();
+    }
+
+    private void Record()
+    {
+        int frame = Time.frameCount % bufferFrames;
+        float time = Time.time;
+        keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+    }
+
+    void PlayBack ()
+    {
+        rigidBody.isKinematic = true;
+        int frame = Time.frameCount % bufferFrames;
+        print("Reading frame " + frame);
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+    }
+}
+
+public struct MyKeyFrame
+{
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame (float time, Vector3 pos, Quaternion rot)
+    {
+        frameTime = time;
+        position = pos;
+        rotation = rot;
+    }
+}
